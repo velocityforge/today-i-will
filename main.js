c6ee4bbf8a78e98ccf064444aac4b9fcb939c784
unknown <matthew.hajec02@gmail.com>
2023-01-12 19:52:40 -0500

    made text only persist for 24 hours

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const textbox = document.getElementById('textbox');
 
-// initialize textbox value from localStorage
-textbox.textContent = localStorage.getItem('textbox') || '';
+// initialize textbox value from localStorage if it is not more than 24 hours old
+const stored = localStorage.getItem('textbox');
+if (stored) {
+  let text, dateTm;
+  try {
+    const parsed = JSON.parse(stored)
+    text = parsed.text;
+    dateTm = new Date(parsed.dateTm);
+  } catch (e) {
+    console.log("Corrupted localStorage data, clearing it")
+    localStorage.setItem('textbox', '');
+  }
+  
+  if (new Date() - dateTm < 24 * 60 * 60 * 1000) {
+    textbox.textContent = text;
+  }
+}
 
 textbox.addEventListener('input', e => {
-  localStorage.setItem('textbox', e.target.textContent);
+  localStorage.setItem('textbox', JSON.stringify({
+    text: e.target.textContent,
+    dateTm: new Date()
+  }));
 });
 
